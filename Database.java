26ee299bfd66833cfcdf2099d821acac7b855c87
imadrid181 <imadrid181@gmail.com>
2019-01-04 02:47:38 -0700

    Final

--- Database.java
+++ Database.java
@@ -1,17 +1,34 @@
 import java.util.LinkedList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
-public class Database{
-    private LinkedList listObject;
+public class Database {
+    private LinkedList<Object> listObject;
     private int spaceOccupied;
     private Boolean isEmpty;
+    private File storage;
+    private FileWriter fw;
+    private BufferedWriter output;
+    private FileReader fr;
+    private BufferedReader reader;
 
-    public Database(){
+    public Database(String fileName) throws IOException{
         this.listObject = new LinkedList<Object>();
         this.spaceOccupied = 0;
         this.isEmpty = false;
+        this.storage = new File(fileName+".txt");
+        this.fw = new FileWriter(storage, true);
+        this.output = new BufferedWriter(fw);
+        this.fr = new FileReader(storage);
+        this.reader =  new BufferedReader(fr);
     }
 
-    public boolean put(Object o){
+    public boolean put(Object o) throws IOException{
         if(o == null) {
             return false; 
         }
@@ -23,11 +40,14 @@ public class Database{
         this.listObject.add(o);
         this.spaceOccupied++;
         this.isEmpty = false;
+        this.output.write(o.toString());
+        this.output.write(System.lineSeparator());
+        this.output.flush();
         return true;
     }
 
 
-    public boolean contains(Object o){
+    public boolean contains(Object o) throws IOException{
         if(o.equals(null))
             return false;
         
@@ -41,13 +61,40 @@ public class Database{
             return false;
     }
 
-    public boolean remove(Object o){
+    public boolean remove(Object o) throws IOException{
         if(o.equals(null))
             return false;
 
         if(!this.listObject.contains(o))
             return false;
         else {
+            File originalFile = new File("Temp.txt");
+            BufferedWriter originalFileWriter = new BufferedWriter(new FileWriter(originalFile));
+            String tempLine;
+            while((tempLine = reader.readLine()) != null){
+                String line = tempLine.trim();
+                originalFileWriter.write(line);
+                originalFileWriter.write(System.lineSeparator());
+                originalFileWriter.flush();
+            }
+            originalFileWriter.close();
+            
+            BufferedReader originalFileReader = new BufferedReader(new FileReader(originalFile));
+            File tempFile = new File(storage.getName());
+            BufferedWriter tempWritter = new BufferedWriter(new FileWriter(tempFile));
+            String lineToRemove = o.toString();
+            String currentLine;
+
+            while((currentLine = originalFileReader.readLine()) != null){
+                String line = currentLine.trim();
+                if(!line.equals(lineToRemove)) {
+                    tempWritter.write(currentLine);
+                    tempWritter.write(System.lineSeparator());
+                }
+            }
+            tempWritter.close();
+            tempFile.renameTo(this.storage);
+            this.storage = tempFile;
             this.listObject.remove(o);
             this.spaceOccupied--;
             return true;
@@ -61,6 +108,33 @@ public class Database{
     
     public Object get(int position){
         return this.listObject.get(position);
+    }
+
+    public int populateFromFile(String FileName) throws IOException{
+        try{
+            File inputFile = new File(FileName);
+            BufferedReader tempReader = new BufferedReader(new FileReader(inputFile));
+            int counter = 0;
+
+            String currentLine;
+            while((currentLine = tempReader.readLine()) != null){
+                String[] values = currentLine.split(",");
+                if(values.length > 1){
+                    EBook temp = new EBook(values, counter);
+                    this.listObject.add(temp);
+                    this.spaceOccupied++;
+                    counter++;
+                }
+                else {
+                    this.listObject.add(currentLine);
+                    this.spaceOccupied++;
+                }
+            }
+            tempReader.close();
+            return counter;
+        }catch(FileNotFoundException e){
+            return 0;
+        }
     }
 
 
